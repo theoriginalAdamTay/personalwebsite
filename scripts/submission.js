3bf19714486dee7411333714a041219c31ae5789
theoriginalAdamTay <117193548+theoriginalAdamTay@users.noreply.github.com>
2024-09-14 00:39:13 +1000

    Added implementation of contact form
    Added implementation of contact form including spam protection question, and changed font on portfolio gallery for easier readability.

---
+++ scripts/submission.js
@@ -0,0 +1,13 @@
+function submitForm() {
+    var spamProtectField = document.getElementById('spamProtection').value;
+    const spamProtectAnswer = "Connor Rozee"
+    if (spamProtectField.toUpperCase() != spamProtectAnswer.toUpperCase()) {
+        alert("Validation failed, please try again");
+        document.getElementById('contactFormSubmit').disabled = true;
+        document.getElementById('contactForm').reset();
+    }
+    else {
+        alert("Validation successful!");
+        window.open('mailto:ogadamtay313@gmail.com?subject=Message%20from%20a%20website%20user')
+    }
+}
